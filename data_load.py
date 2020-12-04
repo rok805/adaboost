9e7c2644d5e6003974cf601845ab8b4f5281d2c3
rok <37019118+rok805@users.noreply.github.com>
2020-12-04 12:00:12 +0900

    first commit

---
+++ data_load.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  4 11:31:55 2020
+
+@author: user
+"""
+
+import pandas as pd
+
+diabetes = pd.read_csv('data/diabetes_csv.csv', header=None)
+liver = pd.read_csv('data/Indian Liver Patient Dataset (ILPD).csv', header=None)
+ionosphere = pd.read_table('data/ionosphere_data.txt', sep=',', header=None).iloc[0,:]
+ionos = []
+for i in ionosphere:
+    ionos.append(i.split(','))
+ionosphere = pd.DataFrame(ionos)    
+del ionos
+
